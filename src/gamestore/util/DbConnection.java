6c2c36e4c5a5e12e6a9a87659723a09cb07d5b9e
Thiago Silva Lopes <thiago.lopes@scaleup.com.br>
2023-05-05 03:59:20 -0300

    Repaired database name

--- src/gamestore/util/DbConnection.java
+++ src/gamestore/util/DbConnection.java
@@ -8,7 +8,7 @@ public class DbConnection {
     public Connection getConnection() throws SQLException, ClassNotFoundException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/projetomusica";
+            String url = "jdbc:mysql://localhost:3306/gamestore";
             String user = "root";
             // String senha = "admin";
             return DriverManager.getConnection(url, user, null);
