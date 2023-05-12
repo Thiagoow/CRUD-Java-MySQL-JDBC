ac0d63edc7e484e6a504a83861a3581896e2f8a5
Thiago Silva Lopes <thiago.lopes@scaleup.com.br>
2023-05-12 01:08:49 -0300

    Repaired sql commands - Renamed customers table to clients

--- gamestore.sql
+++ gamestore.sql
@@ -4,7 +4,7 @@ create database gamestore;
 
 USE gamestore;
 
-create table gamestore.customers (
+create table gamestore.clients (
   id BIGINT NOT NULL AUTO_INCREMENT,
   username VARCHAR(255),
   password VARCHAR(255),
@@ -29,11 +29,11 @@ create table gamestore.purchases (
   id_customers BIGINT
 );
 
-ALTER TABLE gamestore.purchases ADD CONSTRAINT FK_PURCHASESCUSTOMERS_ID FOREIGN KEY (id_customers) REFERENCES customers (ID);
+ALTER TABLE gamestore.purchases ADD CONSTRAINT FK_PURCHASESCUSTOMERS_ID FOREIGN KEY (id_customers) REFERENCES clients (ID);
 ALTER TABLE gamestore.purchases ADD CONSTRAINT FK_PURCHASESGAMES_ID FOREIGN KEY (id_games) REFERENCES games (ID);
 
-INSERT INTO `gamestore`.`customers` (`username`, `password`, `name`, `cpf`, `email`) VALUES ('Thi', '123456', 'Thiago Lopes', '123456789', 'thiagolopes@email.com');
-INSERT INTO `gamestore`.`customers` (`username`, `password`, `name`, `cpf`, `email`) VALUES ('Gui', '123456', 'Guilherme Silva', '9123456789', 'guisilva@email.com');
+INSERT INTO `gamestore`.`clients` (`username`, `password`, `name`, `cpf`, `email`) VALUES ('Thi', '123456', 'Thiago Lopes', '123456789', 'thiagolopes@email.com');
+INSERT INTO `gamestore`.`clients` (`username`, `password`, `name`, `cpf`, `email`) VALUES ('Gui', '123456', 'Guilherme Silva', '9123456789', 'guisilva@email.com');
 
 INSERT INTO `gamestore`.`games` (`name`, `value`, `type`) VALUES ('Red Dead Redemption 2', 400, 'PC');
 INSERT INTO `gamestore`.`games` (`name`, `value`, `type`) VALUES ('God Of War', 200, 'PS4');
